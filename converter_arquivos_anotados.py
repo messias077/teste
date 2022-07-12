fe091852545df7554273eeb61936c0de6815ab43
messias077@yahoo.com.br <messias077@yahoo.com.br>
2022-07-12 13:42:21 -0300

    Rotina para só apagar o DB caso tenha arquivo para processar. Inclusão de main.

--- converter_arquivos_anotados.py
+++ converter_arquivos_anotados.py
@@ -8,6 +8,9 @@
 from src.utils import suprimir_warning_tf
 from src.modulos.ren.conversor_jsonl_conll import converter_jsonl_conll
 
-# Converte os arquivos anotados para o formato CONLL
-converter_jsonl_conll(retirar_sentencas_semelhantes=True, escopo_global_sentencas=True, tamanho_dataset_teste=0.3,
-                      concatenar_arquivos=True)
+# Obs.: para rodar este script diretamente no caminho dele, tem que configurar a variável PYTHONPATH com o caminho do
+# projeto. Exemplo no Linux: export PYTHONPATH='/dados/develop/PycharmProjects/mestrado'
+if __name__ == "__main__":
+    # Converte os arquivos anotados para o formato CONLL
+    converter_jsonl_conll(retirar_sentencas_semelhantes=True, escopo_global_sentencas=True, tamanho_dataset_teste=0.3,
+                          concatenar_arquivos=True)
--- gerar_dataset_anotacao.py
+++ gerar_dataset_anotacao.py
@@ -9,7 +9,7 @@
 import os
 import time
 from src.utils import suprimir_warning_tf
-from src.ambiente.parametros_globais import PREPROC_CAMINHO_ARQ_CONF
+from src.ambiente.parametros_globais import PREPROC_CAMINHO_ARQ_CONF, FILE_NOT_FOUND_ERROR, PERMISSION_ERROR
 from src.ambiente.preparar_ambiente import inicializar_parametros, validar_pastas
 from interface_prog import cadastrar_arquivos
 from src.modulos.preproc.preproc import pre_processamento
@@ -20,55 +20,90 @@ from src.modulos.ren.ren import ren
 # Se não apagar, não será possível cadastrar um edital mais de uma vez, o script não deixa!
 # Não tem uma interface para esta atividade porque ainda não foi decidido se o usuário terá
 # permissão para apagar os editais processados.
-drop_databases = True
+def verificar_arquivos_dropar_databases(caminho):
+    """
+    Verifica se tem arquivos para processar. Caso positivo, limpa a base de dados, caso negativo, aborta o programa.
+        :param caminho: Caminhos onde se encontram os possíveis arquivos para processar.
+    """
+    arquivos_verificar = []
 
-if drop_databases:
-    from pymongo import MongoClient
-    from pymongo.errors import ConnectionFailure
-    from src.ambiente.parametros_globais import CONNECTION_ERROR
-
-    # Conecta ao banco
-    conexao = MongoClient('localhost', 27017)
-
+    # Obtém a lista de todos os arquivos que serão verificados
     try:
-        conexao.admin.command('ismaster')
-    except ConnectionFailure:
-        print('\nErro ao conectar ao banco de dados MongoDB. Faça testes e verifique:')
-        print('- se o nome do servidor está correto;')
-        print('- se a porta para conexão ao banco está correta;')
-        print('- se o banco está rodando e aceitando conexões.\n')
-        exit(CONNECTION_ERROR)
+        arquivos_verificar = os.listdir(caminho)
+    except FileNotFoundError:
+        print(f"\nO caminho '{caminho}' não existe!\n")
+        exit(FILE_NOT_FOUND_ERROR)
+    except PermissionError:
+        print(f"\nErro ao listar o caminho '{caminho}'. Permissão de leitura negada!\n")
+        exit(PERMISSION_ERROR)
 
-    # Apaga os bancos de dados
-    conexao.drop_database('db_documentos')
-    conexao.drop_database('db_metadados')
+    tem_arquivos_para_processar = False
 
-    conexao.close()
+    # Verifica se tem algum PDF para processar
+    for a in arquivos_verificar:
+        if ".pdf" in a.lower():
+            tem_arquivos_para_processar = True
+            break
 
-# Obtém o caminho do arquivo de configuração para montar o caminho completo
-caminho_arquivo_configuracao = PREPROC_CAMINHO_ARQ_CONF
-nome_arquivo_configuracao = 'param_preproc.conf'
-arq_conf = os.path.join(caminho_arquivo_configuracao, nome_arquivo_configuracao)
+    if tem_arquivos_para_processar:
+        from pymongo import MongoClient
+        from pymongo.errors import ConnectionFailure
+        from src.ambiente.parametros_globais import CONNECTION_ERROR
 
-conf = inicializar_parametros('preproc', arq_conf)
+        # Conecta ao banco
+        conexao = MongoClient('localhost', 27017)
 
-# Obtém alguns parâmetros de configuração do módulo
-parametros = {'caminho_arq_conf': arq_conf,
-              'p_caminho_entrada': conf.obter_valor_parametro('p_caminho_entrada')}
+        try:
+            conexao.admin.command('ismaster')
+        except ConnectionFailure:
+            print('\nErro ao conectar ao banco de dados MongoDB. Faça testes e verifique:')
+            print('- se o nome do servidor está correto;')
+            print('- se a porta para conexão ao banco está correta;')
+            print('- se o banco está rodando e aceitando conexões.\n')
+            exit(CONNECTION_ERROR)
 
-validar_pastas(parametros)
+        # Apaga os bancos de dados
+        conexao.drop_database('db_documentos')
+        conexao.drop_database('db_metadados')
 
-# Cadastra todos os editais que estão na pasta
-print("\n### CADASTRO DOS EDITAIS")
-cadastrar_arquivos('EDITAL', 'SISTEMA', parametros['p_caminho_entrada'])
-time.sleep(3)  # Dá um tempo para evitar erros...
+        conexao.close()
+    else:
+        print(f"\nO caminho '{caminho}' não possui arquivos para processar!\n")
+        exit(FILE_NOT_FOUND_ERROR)
 
-# Pré-processa os editais
-print("\n### PRÉ-PROCESSAMENTO")
-pre_processamento()
-time.sleep(5)  # Dá um tempo para o banco de dados indexar os editais inseridos...
 
-# Gera os datasets para enviar para os anotadores
-print("\n### GERAÇÃO DOS DATASETS")
-ren('ner', 100, reprocessar=False, gerar_estatisticas=True, organizar_em_pastas=True,
-    retirar_sentencas_semelhantes=True, escopo_global_sentencas=False)
+# Obs.: para rodar este script diretamente no caminho dele, tem que configurar a variável PYTHONPATH com o caminho do
+# projeto. Exemplo no Linux: export PYTHONPATH='/dados/develop/PycharmProjects/mestrado'
+if __name__ == "__main__":
+    # Obtém o caminho do arquivo de configuração para montar o caminho completo
+    caminho_arquivo_configuracao = PREPROC_CAMINHO_ARQ_CONF
+    nome_arquivo_configuracao = 'param_preproc.conf'
+    arq_conf = os.path.join(caminho_arquivo_configuracao, nome_arquivo_configuracao)
+
+    conf = inicializar_parametros('preproc', arq_conf)
+
+    # Obtém alguns parâmetros de configuração do módulo
+    parametros = {'caminho_arq_conf': arq_conf,
+                  'p_caminho_entrada': conf.obter_valor_parametro('p_caminho_entrada')}
+
+    validar_pastas(parametros)
+
+    # Se existir arquivo para processar, faz a limpeza da base de dados.
+    # Obs.: Esta ação é específica para realizar testes... pois não é interessante ficar reprocessando os editais
+    # e gravando os editais no banco cada vez que for gerar os datasets para anotação!
+    verificar_arquivos_dropar_databases(parametros['p_caminho_entrada'])
+
+    # Cadastra todos os editais que estão na pasta
+    print("\n### CADASTRO DOS EDITAIS")
+    cadastrar_arquivos('EDITAL', 'SISTEMA', parametros['p_caminho_entrada'])
+    time.sleep(3)  # Dá um tempo para evitar erros...
+
+    # Pré-processa os editais
+    print("\n### PRÉ-PROCESSAMENTO")
+    pre_processamento()
+    time.sleep(5)  # Dá um tempo para o banco de dados indexar os editais inseridos...
+
+    # Gera os datasets para enviar para os anotadores
+    print("\n### GERAÇÃO DOS DATASETS")
+    ren('ner', 100, reprocessar=False, gerar_estatisticas=True, organizar_em_pastas=True,
+        retirar_sentencas_semelhantes=True, escopo_global_sentencas=False)
